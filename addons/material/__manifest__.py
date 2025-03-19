dfe8878963f970649b6d83351d4ad60f76a971bb
nurfachridaffa17 <nurfachridaffa17@gmail.com>
2025-03-19 09:23:40 +0700

    [FEAT] : Add module web responsive

---
+++ addons/material/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Material Module',
+    'version': '1.0',
+    'description': 'Material Module For Test Backend Developer',
+    'summary': 'Material Module For Test Backend Developer',
+    'author': 'Nurfachri Daffa Alhakim',
+    'license': 'LGPL-3',
+    'category': 'Product',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'assets': {
+    },
+
+    'auto_install': False,
+    'application': False,
+    'installable': True
+}
